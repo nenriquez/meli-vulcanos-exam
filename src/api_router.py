bbe60e0a87812d28324d1565f4a3905c64cc1657
Nicolas Enriquez <nico@kidloom.com>
2016-12-19 18:43:56 -0300

    fixes

--- src/api_router.py
+++ src/api_router.py
@@ -1,9 +1,10 @@
 import webapp2
+from webapp2 import Route
 
 from api.run_job_handler import RunJobHandler
 from api.weather_handler import WeatherHandler
 
 routes = webapp2.WSGIApplication([
-    ('/clima', WeatherHandler)
-    ('/run_job', RunJobHandler)
+    Route('r/clima', WeatherHandler),
+    Route('r/run_job', RunJobHandler)
 ], debug=True)
